48ef1718add0aaab9397f297851bf43399a8f378
StefanCitiriga <57890672+StefanCitiriga@users.noreply.github.com>
2023-10-10 20:18:56 +0300

    Initial commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  //useState is a react hook;
+  //all hooks must be called in the component function
+
+  const clickHandler = (props) => {
+    setTitle("Updated!"); //this is scheduled, not instant
+    console.log(title); //this is instant, not scheduled
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
